a84bb1da3fa086accd38b03caede09ea51ea83d1
Marcos52000 <55589715+Marcos52000@users.noreply.github.com>
2022-01-24 09:45:48 +0100

    MILESTONE2

---
+++ M3/src/Clases/Conductor.java
@@ -0,0 +1,10 @@
+package Clases;
+
+public class Conductor extends Persona {
+	
+	// constructor
+	
+	public Conductor(String nombre, String apellido, String dataNaixement, Llicencia tipoLlicencia) {
+		super(nombre, apellido, dataNaixement, tipoLlicencia);
+	}
+}
--- M3/src/Main/VehiculoApp.java
+++ M3/src/Main/VehiculoApp.java
@@ -1,18 +1,19 @@
 package Main;
 
+import Clases.*;
 import javax.swing.JOptionPane;
+import Interfaz.PedirDatos;
 
-import Clases.Cotxe;
-import Clases.Rueda;
-import Interfaz.*;
-
-public class VehiculoApp {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String vehiculo = JOptionPane.showInputDialog("Que quieres crear cotxe o moto");
-		PedirDatos.crearVehiculo(PedirDatos.pedirDatos(), vehiculo);
-		
-	}
-
+public class VehiculoApp
+{
+    public static void main(final String[] args) {
+        final Titular t1 = PedirDatos.pedirTitular();
+        final String vehiculo = JOptionPane.showInputDialog("Que quieres crear cotxe / moto / camion");
+        PedirDatos.seleccionVehiculo(vehiculo, t1);
+        String conduce = JOptionPane.showInputDialog("EL titular es el conductor del vehiculo? si/no");
+        if(conduce.equals("no")) {
+        	Conductor c1 = PedirDatos.pedirConductor();
+        	PedirDatos.seleccionVehiculo(vehiculo, t1);
+        }
+    }
 }
